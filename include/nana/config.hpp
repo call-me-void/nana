0843f6035b31d877a783ba907f2f7bdf311ecc92
mixael <m_shevcov@mail.ru>
2021-12-18 00:26:58 +0300

    nana latest check

--- include/nana/config.hpp
+++ include/nana/config.hpp
@@ -25,6 +25,8 @@
 
 #include "c++defines.hpp"
 
+#define NANA_LATEST
+
 //The following basic configurations are ignored when NANA_IGNORE_CONF is defined.
 //The NANA_IGNORE_CONF may be specified by CMake generated makefile.
 #ifndef NANA_IGNORE_CONF
